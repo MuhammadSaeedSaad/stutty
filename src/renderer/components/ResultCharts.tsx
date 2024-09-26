03b1fc4f7687220cbcc06da18b5e592d3a728d5d
MuhammadSaeedSaad <mouhammedsaed@gmail.com>
2024-09-26 13:48:37 +0300

    started to edit the styling

--- src/renderer/components/ResultCharts.tsx
+++ src/renderer/components/ResultCharts.tsx
@@ -3,9 +3,9 @@ import BarChart from './BarChart';
 
 export default function ResultsCharts({ fluentData, dysFluentData }) {
   return (
-    <div>
+    <>
       <BarChart chartingData={dysFluentData} />
       <BarChart chartingData={fluentData} />
-    </div>
+    </>
   );
 }
